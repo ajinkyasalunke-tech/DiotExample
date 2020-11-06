4fb990214e460974970de9ad87cd59bc792b4bf8
Ajinkya <ajsalunke1998@gmail.com>
2020-11-06 12:07:26 +0530

    Adding 2 function

--- example.java
+++ example.java
@@ -1,9 +1,14 @@
 class example{
     public static void printEvenNumbers(int no) {
-System.out.prinln("Even nimbers upto "+ no);
+System.out.prinln("Even numbers upto "+ no);
 for (int x=0; x<no;x=x+2) {
     System.out.prinln(x);
-    
+
+    System.out.prinln("Calling the odd number");
+    firstexample.name(11);
+
+    System.out.prinln("Calling the odd number");
+    thirdexample.name(12);
 }
         
     }
